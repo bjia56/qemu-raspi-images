6ed2b741aa839c942fe1fd464fec360242adfd66
Brett Jia <dev.bjia56@gmail.com>
2023-04-09 00:18:38 -0400

    Update run_vm.sh

--- files/run_vm.sh
+++ files/run_vm.sh
@@ -11,8 +11,12 @@ TAP="tap0"
 INTERFACE="eth0"
 BRIDGE_IP="172.18.0.1/16"
 
+
 echo "Adding bridge $BRIDGE"
 ip link add name $BRIDGE type bridge
+
+echo "Flushing interface $INTERFACE"
+ip addr flush dev $INTERFACE
 
 echo "Setting $BRIDGE as master of $INTERFACE"
 ip link set $INTERFACE master $BRIDGE
@@ -23,12 +27,24 @@ ip tuntap add $TAP mode tap
 echo "Setting $BRIDGE as master of $TAP"
 ip link set $TAP master $BRIDGE
 
-echo "Setting $BRIDGE and $TAP up"
+echo "Setting $INTERFACE, $BRIDGE and $TAP up"
+ip link set up dev $INTERFACE
 ip link set up dev $TAP
 ip link set up dev $BRIDGE
 
-echo "Setting $BRIDGE IP to $BRIDGE_IP"
-ip addr add $BRIDGE_IP dev $BRIDGE
+echo "Stopping NetworkManager"
+systemctl stop NetworkManager
+
+echo "Requesting ip for $BRIDGE"
+dhclient $BRIDGE
+
+if [ $? -eq 0 ]; then
+    echo "Requesting ip for $INTERFACE"
+    dhclient $INTERFACE
+    echo "Killing dhclient and starting NetworkManager"
+    pkill -9 dhclient
+    systemctl start NetworkManager
+fi
 
 ip addr
 
