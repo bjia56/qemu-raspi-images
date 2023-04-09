230e6c8ca1107e479aebfab2da94a2fef74486f9
Brett Jia <dev.bjia56@gmail.com>
2023-04-08 22:22:10 -0400

    try fix vm device naming

--- files/run_vm.sh
+++ files/run_vm.sh
@@ -35,7 +35,7 @@ ip addr
 qemu-system-aarch64 \
     -M raspi3b \
     -cpu cortex-a72 \
-    -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+    -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1 net.ifnames=0 biosdevname=0" \
     -dtb bcm2710-rpi-3-b-plus.dtb \
     -drive if=sd,index=0,file=raspi.img,format=raw \
     -kernel kernel8.img \
