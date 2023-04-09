938627889e874599e2148ba4d027b6fd228e5ad7
Brett Jia <dev.bjia56@gmail.com>
2023-04-08 21:27:18 -0400

    wip tap networking

--- files/run_vm.sh
+++ files/run_vm.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
+
+# networking adapted from https://gist.github.com/extremecoders-re/e8fd8a67a515fee0c873dcafc81d811c?permalink_comment_id=4328259#gistcomment-4328259
+if [[ $EUID > 0 ]]
+  then echo "Run this script as root"
+  exit
+fi
+
+BRIDGE="br0"
+TAP="tap0"
+INTERFACE="enp7s0"
+
+echo "Adding bridge $BRIDGE"
+ip link add name $BRIDGE type bridge
+
+echo "Flushing interface $INTERFACE"
+ip addr flush dev $INTERFACE
+
+echo "Setting $BRIDGE as master of $INTERFACE"
+ip link set $INTERFACE master $BRIDGE
+
+echo "Adding tap $TAP"
+ip tuntap add $TAP mode tap
+
+echo "Setting $BRIDGE as master of $TAP"
+ip link set $TAP master $BRIDGE
+
+echo "Setting $INTERFACE, $BRIDGE and $TAP up"
+ip link set up dev $INTERFACE
+ip link set up dev $TAP
+ip link set up dev $BRIDGE
+
+echo "Stopping NetworkManager"
+systemctl stop NetworkManager
+
+echo "Requesting ip for $BRIDGE"
+dhclient $BRIDGE
+
+if [ $? -eq 0 ]; then
+    echo "Requesting ip for $INTERFACE"
+    dhclient $INTERFACE
+    echo "Killing dhclient and starting NetworkManager"
+    pkill -9 dhclient
+    systemctl start NetworkManager
+fi
+
+
+
 qemu-system-aarch64 \
     -M raspi3b \
     -cpu cortex-a72 \
