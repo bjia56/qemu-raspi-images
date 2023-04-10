03a4e477ab3361fc8c1d3037b05066dc7734ee97
Brett Jia <dev.bjia56@gmail.com>
2023-04-10 09:13:46 -0400

    Update install_tools.sh

--- files/install_tools.sh
+++ files/install_tools.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-
-curl google.com
-curl archive.raspberrypi.org
-curl deb.debian.org
-curl get.docker.com
+!/bin/bash
 
 apt update
 apt -y upgrade
 
 # install docker
-curl -fsSL https://get.docker.com | sh
+apt -y install ca-certificates curl gnupg
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
