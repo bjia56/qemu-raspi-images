fb9c672fc9cdb7127bd8cf6037c60bdbe03b22b8
Brett Jia <dev.bjia56@gmail.com>
2023-04-10 08:05:44 -0400

    Restore

--- files/run_vm.sh
+++ files/run_vm.sh
@@ -11,38 +11,38 @@ TAP="tap0"
 INTERFACE="eth0"
 BRIDGE_IP="172.18.0.1/16"
 
-#echo "Adding bridge $BRIDGE"
-#ip link add name $BRIDGE type bridge
-#
-#echo "Setting $BRIDGE as master of $INTERFACE"
-#ip link set $INTERFACE master $BRIDGE
-#
-#echo "Adding tap $TAP"
-#ip tuntap add $TAP mode tap
-#
-#echo "Setting $BRIDGE as master of $TAP"
-#ip link set $TAP master $BRIDGE
-#
-#echo "Setting $BRIDGE IP to $BRIDGE_IP"
-#ip addr add $BRIDGE_IP dev $BRIDGE
-#
-#echo "Setting $INTERFACE, $BRIDGE and $TAP up"
-#ip link set up dev $INTERFACE
-#ip link set up dev $TAP
-#ip link set up dev $BRIDGE
-#
-#echo "Printing interfaces and routes"
-#ip addr
-#ip route
-#
-#echo "Adding iptables rules"
-#iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
-#iptables -P FORWARD ACCEPT
+echo "Adding bridge $BRIDGE"
+ip link add name $BRIDGE type bridge
+
+echo "Setting $BRIDGE as master of $INTERFACE"
+ip link set $INTERFACE master $BRIDGE
+
+echo "Adding tap $TAP"
+ip tuntap add $TAP mode tap
+
+echo "Setting $BRIDGE as master of $TAP"
+ip link set $TAP master $BRIDGE
+
+echo "Setting $BRIDGE IP to $BRIDGE_IP"
+ip addr add $BRIDGE_IP dev $BRIDGE
+
+echo "Setting $INTERFACE, $BRIDGE and $TAP up"
+ip link set up dev $INTERFACE
+ip link set up dev $TAP
+ip link set up dev $BRIDGE
+
+echo "Printing interfaces and routes"
+ip addr
+ip route
+
+echo "Adding iptables rules"
+iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
+iptables -P FORWARD ACCEPT
 
 echo "Starting QEMU"
 qemu-system-aarch64 \
     -M raspi3b \
-    -cpu cortex-a53 \
+    -cpu cortex-a72 \
     -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1 net.ifnames=0 biosdevname=0" \
     -dtb bcm2710-rpi-3-b-plus.dtb \
     -drive if=sd,index=0,file=raspi.img,format=raw \
@@ -50,5 +50,5 @@ qemu-system-aarch64 \
     -m 1G -smp 4 \
     -serial stdio \
     -usb -device usb-net,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::5555-:22 \
+    -netdev tap,id=net0,ifname=$TAP,script=no,downscript=no \
     -display none
