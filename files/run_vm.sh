3240973fbe44f9973fd3dbad46343c796bbce927
Brett Jia <dev.bjia56@gmail.com>
2023-04-10 07:52:42 -0400

    Update run_vm.sh

--- files/run_vm.sh
+++ files/run_vm.sh
@@ -61,7 +61,7 @@ qemu-system-aarch64 -M virt  \
       -cpu max,pauth-impdef=on -smp 2 -m 1G           \
       -drive if=pflash,format=raw,file=efi.img,readonly=on      \
       -drive if=pflash,format=raw,file=varstore.img         \
-      -drive if=virtio,format=qcow2,file=raspi.img           \
+      -drive if=virtio,format=raw,file=raspi.img           \
       -device virtio-scsi-pci,id=scsi0              \
       -object rng-random,filename=/dev/urandom,id=rng0      \
       -device virtio-rng-pci,rng=rng0               \
