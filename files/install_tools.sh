a7ffc53bdd3edbe40eab1684a266f889ef312c52
Brett Jia <dev.bjia56@gmail.com>
2023-04-10 20:30:09 -0400

    add pi to docker group

--- files/install_tools.sh
+++ files/install_tools.sh
@@ -12,4 +12,5 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -a -G docker pi
